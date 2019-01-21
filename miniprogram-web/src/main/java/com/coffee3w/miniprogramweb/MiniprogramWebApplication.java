0181fc30c6d870ed1a9f71f8a83fb8e82d0031bb
nlpeng <nlpeng@outlook.com>
2019-01-21 11:47:43 +0800

    Initial version commit

---
+++ miniprogram-web/src/main/java/com/coffee3w/miniprogramweb/MiniprogramWebApplication.java
@@ -0,0 +1,14 @@
+package com.coffee3w.miniprogramweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprogramWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniprogramWebApplication.class, args);
+    }
+
+}
+
